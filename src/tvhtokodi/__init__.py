196def550fa10c62ef67366498e6f7e2c6fb96b2
ccdale <chris.allison@hivehome.com>
2023-06-18 07:45:27 +0100

    Bumping version from 0.1.0 to 0.1.1

---
+++ src/tvhtokodi/__init__.py
@@ -0,0 +1,49 @@
+#
+# Copyright (c) 2023, Chris Allison
+#
+#     This file is part of tvhtokodi.
+#
+#     tvhtokodi is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     tvhtokodi is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with tvhtokodi.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import sys
+
+# import ccalogging
+
+__version__ = "0.1.1"
+__appname__ = "tvhtokodi"
+
+# ccalogging.setDebug()
+# ccalogging.setConsoleOut()
+# log = ccalogging.log
+
+
+def errorNotify(exci, e, fname=None):
+    lineno = exci.tb_lineno
+    if fname is None:
+        fname = exci.tb_frame.f_code.co_name
+    ename = type(e).__name__
+    msg = f"{ename} Exception at line {lineno} in function {fname}: {e}"
+    # log.error(msg)
+    print(msg)
+
+
+def errorRaise(exci, e, fname=None):
+    errorNotify(exci, e, fname)
+    raise
+
+
+def errorExit(exci, e, fname=None):
+    errorNotify(exci, e, fname)
+    sys.exit(1)
---
+++ tests/test_tvhtokodi.py
@@ -0,0 +1,89 @@
+#
+# Copyright (c) 2023, Chris Allison
+#
+#     This file is part of tvhtokodi.
+#
+#     tvhtokodi is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     tvhtokodi is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with tvhtokodi.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import sys
+
+import pytest
+
+from tvhtokodi import errorExit, errorNotify, errorRaise, __version__
+
+
+class TheException(Exception):
+    """A test Exception.
+    Args:
+        Exception:
+    """
+
+    pass
+
+
+def test_tvhtokodi_version():
+    assert __version__ == "0.1.1"
+
+
+def test_errorNotify(capsys):
+    try:
+        msg = "This is the test exception"
+        raise TheException(msg)
+    except Exception as e:
+        exci = sys.exc_info()[2]
+        lineno = exci.tb_lineno
+        fname = exci.tb_frame.f_code.co_name
+        ename = type(e).__name__
+        errorNotify(exci, e)
+    finally:
+        emsg = f"{ename} Exception at line {lineno} in function {fname}: {msg}\n"
+        out, err = capsys.readouterr()
+        assert out == emsg
+
+
+def test_errorRaise(capsys):
+    """It raises the TheException Exception after printing the error."""
+    try:
+        msg = "This is the test exception"
+        raise TheException(msg)
+    except Exception as e:
+        exci = sys.exc_info()[2]
+        lineno = exci.tb_lineno
+        fname = exci.tb_frame.f_code.co_name
+        ename = type(e).__name__
+        with pytest.raises(TheException):
+            errorRaise(exci, e)
+    finally:
+        emsg = f"{ename} Exception at line {lineno} in function {fname}: {msg}\n"
+        out, err = capsys.readouterr()
+        assert out == emsg
+
+
+def test_errorExit(capsys):
+    """It attempts sys.exit after printing the error."""
+    try:
+        msg = "This is the test exception"
+        raise TheException(msg)
+    except Exception as e:
+        exci = sys.exc_info()[2]
+        lineno = exci.tb_lineno
+        fname = exci.tb_frame.f_code.co_name
+        ename = type(e).__name__
+        with pytest.raises(SystemExit):
+            errorExit(exci, e)
+    finally:
+        emsg = f"{ename} Exception at line {lineno} in function {fname}: {msg}\n"
+        out, err = capsys.readouterr()
+        assert out == emsg
