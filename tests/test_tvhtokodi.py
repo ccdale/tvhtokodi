1e9a2f02b2b9e36edf07499719692ef23f7d8c79
ccdale <chris.allison@hivehome.com>
2023-07-01 09:42:27 +0100

    Bumping version from 0.1.7 to 0.1.8

--- tests/test_tvhtokodi.py
+++ tests/test_tvhtokodi.py
@@ -35,7 +35,7 @@ class TheException(Exception):
 
 
 def test_tvhtokodi_version():
-    assert tvhtokodi.__version__ == "0.1.7"
+    assert tvhtokodi.__version__ == "0.1.8"
 
 
 def test_errorNotify(capsys):
