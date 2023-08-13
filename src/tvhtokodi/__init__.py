eeeb299bd87740cedb139ca23b7a5e70ccb00a26
ccdale <chris.allison@hivehome.com>
2023-08-13 15:34:08 +0100

    Bumping version from 0.2.13 to 0.2.14

--- src/tvhtokodi/__init__.py
+++ src/tvhtokodi/__init__.py
@@ -21,7 +21,7 @@ from pathlib import Path
 import sys
 
 
-__version__ = "0.2.13"
+__version__ = "0.2.14"
 __appname__ = "tvhtokodi"
 
 configfn = Path.home().joinpath(".config", f"{__appname__}.json")
