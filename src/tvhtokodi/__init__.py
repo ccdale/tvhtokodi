efa41ce27ddc2b835ae21e255b0c0f731334c96a
ccdale <chris.allison@hivehome.com>
2023-08-19 09:10:57 +0100

    Bumping version from 0.2.15 to 0.3.0

--- src/tvhtokodi/__init__.py
+++ src/tvhtokodi/__init__.py
@@ -21,7 +21,7 @@ from pathlib import Path
 import sys
 
 
-__version__ = "0.2.15"
+__version__ = "0.3.0"
 __appname__ = "tvhtokodi"
 
 configfn = Path.home().joinpath(".config", f"{__appname__}.json")
--- tests/test_tvhtokodi.py
+++ tests/test_tvhtokodi.py
@@ -35,7 +35,7 @@ class TheException(Exception):
 
 
 def test_tvhtokodi_version():
-    assert tvhtokodi.__version__ == "0.2.15"
+    assert tvhtokodi.__version__ == "0.3.0"
 
 
 def test_errorNotify(capsys):
