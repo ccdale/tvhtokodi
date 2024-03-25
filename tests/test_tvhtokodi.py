afd2e6ea69065fe1bbe900edc087be3e684dea13
ccdale <chris.charles.allison+git@gmail.com>
2024-03-25 20:41:57 +0000

    Bumping version from 0.3.10 to 0.3.11

--- tests/test_tvhtokodi.py
+++ tests/test_tvhtokodi.py
@@ -35,7 +35,7 @@ class TheException(Exception):
 
 
 def test_tvhtokodi_version():
-    assert tvhtokodi.__version__ == "0.3.10"
+    assert tvhtokodi.__version__ == "0.3.11"
 
 
 def test_errorNotify(capsys):
