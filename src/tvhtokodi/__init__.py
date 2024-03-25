5be851cf2bcb4ce665a6a39df1c1982dc72230fb
ccdale <chris.charles.allison+git@gmail.com>
2024-03-25 09:58:49 +0000

    Bumping version from 0.3.4 to 0.3.5

--- src/tvhtokodi/__init__.py
+++ src/tvhtokodi/__init__.py
@@ -21,7 +21,7 @@ from pathlib import Path
 import sys
 
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 __appname__ = "tvhtokodi"
 
 configfn = Path.home().joinpath(".config", f"{__appname__}.json")
--- tests/test_tvhtokodi.py
+++ tests/test_tvhtokodi.py
@@ -35,7 +35,7 @@ class TheException(Exception):
 
 
 def test_tvhtokodi_version():
-    assert tvhtokodi.__version__ == "0.3.4"
+    assert tvhtokodi.__version__ == "0.3.5"
 
 
 def test_errorNotify(capsys):
