b31636d1f2de2737f6c33523ed7aa50d86c7a42d
ccdale <chris.charles.allison+git@gmail.com>
2024-03-25 09:56:48 +0000

    send each title one by one, add ignore titles

--- src/tvhtokodi/gui.py
+++ src/tvhtokodi/gui.py
@@ -37,7 +37,10 @@ log = None
 def displayTitles(titles):
     try:
         op = []
+        sent = []
+        ignored = tvhtokodi.cfg["ignoretitles"]
         names = sorted([n for n in titles])
+        names = refreshNames(names, sent)
         titleslist = sg.Listbox(
             names,
             size=(60, 40),
@@ -71,11 +74,31 @@ def displayTitles(titles):
                             )
                             title["nfo"] = nfo
                             op.append(title)
+                        sendNextFile(op)
+                        sent.append(op[0]["title"])
+                    op = []
+                    ignored = tvhtokodi.cfg["ignoretitles"]
+                    names = refreshNames(names, sent)
+                    wind["-TLIST-"].update(values=names)
                     log.debug(f"{selection[0]=}")
         wind.close()
-        return op
+        # return op
     except Exception as e:
         errorNotify(sys.exc_info()[2], e)
+
+
+def refreshNames(names, sent):
+    try:
+        ignored = tvhtokodi.cfg["ignoretitles"]
+        for n in sent:
+            if n in names:
+                names.remove(n)
+        for n in ignored:
+            if n in names:
+                names.remove(n)
+        return names
+    except Exception as e:
+        errorRaise(sys.exc_info()[2], e)
 
 
 def decideDestFn(title, xdest, seriesfolders):
@@ -112,7 +135,8 @@ def progWindow(show, total=1):
             tmp = textwrap.wrap(desc)
             desc = "\n".join(tmp)
         msg = f"{total} programme" if total == 1 else f"{total} programmes"
-        legend = "Move" if total == 1 else f"Move {total} Programmes"
+        # legend = "Move" if total == 1 else f"Move {total} Programmes"
+        legend = f"Move {msg}"
         checked = True if show["season"] and show["episode"] else False
         layout = [
             [sg.Text(f'{show["ctimestart"]} Duration: {hmsDisplay(show["duration"])}')],
@@ -130,7 +154,13 @@ def progWindow(show, total=1):
                     ),
                 ]
             ],
-            [[sg.Button(legend, key="-MOVE-"), sg.Quit()]],
+            [
+                [
+                    sg.Button(legend, key="-MOVE-"),
+                    sg.Button("Ignore Show", key="-IGNORE-"),
+                    sg.Cancel(),
+                ]
+            ],
         ]
         wind = sg.Window(show["title"], layout)
         seriesfolders = False
@@ -138,6 +168,12 @@ def progWindow(show, total=1):
             event, values = wind.read()
             if event in (sg.WIN_CLOSED, "Exit", "Quit"):
                 dest = None
+                break
+            if event == "-IGNORE-":
+                ignoreShow(show["title"])
+                dest = None
+                seriesfolders = False
+                nfo = None
                 break
             if event == "-MOVE-":
                 if values["-YEAR-"] == "Year":
@@ -159,6 +195,27 @@ def progWindow(show, total=1):
         errorNotify(sys.exc_info()[2], e)
 
 
+def ignoreShow(title):
+    try:
+        tvhtokodi.cfg["ignoretitles"].append(title)
+        tvhtokodi.writeConfig()
+    except Exception as e:
+        errorRaise(sys.exc_info()[2], e)
+
+
+def sendNextFile(move):
+    try:
+        fnum = int(tvhtokodi.cfg["filenumber"])
+        opfn = f"/tmp/tvhtokodi-move-{fnum}.json"
+        with open(opfn, "w") as ofn:
+            json.dump(move, ofn, indent=4)
+        sendFileTo(opfn)
+        tvhtokodi.cfg["filenumber"] = f"{fnum + 1}"
+        tvhtokodi.writeConfig()
+    except Exception as e:
+        errorRaise(sys.exc_info()[2], e)
+
+
 def doGui():
     try:
         global log
@@ -172,12 +229,10 @@ def doGui():
         log.setLevel(logging.DEBUG)
         tvhtokodi.readConfig()
         recs, titles = recordedTitles()
-        move = displayTitles(titles)
-        log.debug(f"{move=}")
-        opfn = "/tmp/tvhtokodi-move.json"
-        with open(opfn, "w") as ofn:
-            json.dump(move, ofn, indent=4)
-        sendFileTo(opfn)
+        # move = displayTitles(titles)
+        displayTitles(titles)
+        # log.debug(f"{move=}")
+        # sendNextFile(move)
     except Exception as e:
         errorNotify(sys.exc_info()[2], e)
 
